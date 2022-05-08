8f5862aa0f24d44f336af2439b09e09ea467fbed
dreamonaut <dreamonautre@gmail.com>
2022-05-08 20:30:03 +0500

    Adding another packages

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import * as ReactDOM from 'react-dom/client';
+import React from 'react';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(<h1>Hello, world!</h1>);
